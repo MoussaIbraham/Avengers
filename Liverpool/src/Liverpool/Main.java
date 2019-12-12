14e2d3c2caae094b32e8c8b55250cf139ddee230
enano <enano@DESKTOP-T466GT0>
2019-12-12 10:13:31 +0100

    LOGIN DEFINITIVO

--- Liverpool/src/Liverpool/Main.java
+++ Liverpool/src/Liverpool/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		LoginWindow window = new LoginWindow(mimodelo);
 		window.main(args);
-		System.out.println("hello");
+		System.out.println("hllo");
 	}
 	
 }
