5e08baf096c04887083147eb419b1a0cc159e96a
enano <enano@DESKTOP-T466GT0>
2019-12-12 10:35:12 +0100

    LISTA DIRIGENTES MUESTRA DIRECT Y ARCHIVOS

--- Liverpool/src/Liverpool/ClientFTP.java
+++ Liverpool/src/Liverpool/ClientFTP.java
@@ -9,7 +9,57 @@ import org.apache.commons.net.ftp.*;
 public class ClientFTP {
 	
 	private static Modelo mimodelo;
+	FTPFile[] files;
+	
+	public ClientFTP (String User, String Pass, Modelo mimodelo) {
+		this.mimodelo=mimodelo;
+		
+		// Creando nuestro objeto Client
+		FTPClient client = new FTPClient();
 
+		// Datos para conectar al servidor FTP
+		String ftp = mimodelo.getTextoIpFtp(); // También puede ir la IP
+
+		try {
+			// Conactando al servidor
+			client.connect(ftp);
+
+			// Logueado un usuario (true = pudo conectarse, false = no pudo
+			// conectarse)
+			boolean login = client.login(User, Pass);
+			if (login) {
+				System.out.println(mimodelo.getTextoVentanaemergenteLoginExito()); //Cambiar esto por ventana emergente
+				client.changeWorkingDirectory("/SIGE ODOO");
+				//Obtenemos ficheros y directorios.
+				files = client.listFiles();
+				client.setFileType(FTP.BINARY_FILE_TYPE);
+
+				String filename = "miarchivo.txt";
+				BufferedInputStream in = new BufferedInputStream(
+						new FileInputStream("C:\\Users\\enano\\OneDrive\\Escritorio\\SIGE ODOO\\" + filename));
+				System.out.println(mimodelo.getTextoVentanaEmergenteGestionSubidaExitosa()); //Cambiar esto por ventana emergente
+				// Guardando el archivo en el servidor
+				client.storeFile(filename, in);
+				in.close();
+
+			} else {
+				System.out.println(mimodelo.getTextoVentanaemergenteLoginError()); //Cambiar esto por ventana emergente
+			}
+
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+	
+	public FTPFile[] getFiles() {
+		return files;
+	}
+
+	public void setFiles(FTPFile[] files) {
+		this.files = files;
+	}
+
+	
 	public FTPClient Conexion(String User, String Pass, Modelo mimodelo) {
 		
 		this.mimodelo=mimodelo;
@@ -30,6 +80,7 @@ public class ClientFTP {
 			if (login) {
 				System.out.println(mimodelo.getTextoVentanaemergenteLoginExito()); //Cambiar esto por ventana emergente
 				client.changeWorkingDirectory("/SIGE ODOO");
+				FTPFile[] files = client.listFiles();
 				client.setFileType(FTP.BINARY_FILE_TYPE);
 
 				String filename = "miarchivo.txt";
--- Liverpool/src/Liverpool/DirigentesWindow.java
+++ Liverpool/src/Liverpool/DirigentesWindow.java
@@ -33,43 +33,66 @@ public class DirigentesWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public DirigentesWindow(String string, String pass, Modelo mimodelo) {
-		this.mimodelo=mimodelo;
+		this.mimodelo = mimodelo;
+		ClientFTP cliente = new ClientFTP(string, pass, mimodelo);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 444);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		User = string;
 		this.pass = pass;
-		
+
 		list = new JList();
 		list.setBounds(41, 31, 310, 332);
 		contentPane.add(list);
-		
+		try {
+			LlenarLista(cliente.getFiles(), "/");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		JButton btnSalir = new JButton(mimodelo.getTextoBotonSalir());
 		btnSalir.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {				
-	            LoginWindow login = new LoginWindow(mimodelo);
-	            String[] args = null;
-	            setVisible(false);
+			public void actionPerformed(ActionEvent arg0) {
+				LoginWindow login = new LoginWindow(mimodelo);
+				String[] args = null;
+				setVisible(false);
 				LoginWindow.main(args);
 			}
 		});
 		btnSalir.setBounds(429, 317, 89, 23);
 		contentPane.add(btnSalir);
 	}
-	
-	public static void LlenarLista(FTPFile[] files, String direc2) {
-		if(files == null) return;
-		
-		DefaultListModel lista = new DefaultListModel<>();		
+
+	public static void LlenarLista(FTPFile[] files, String direc2) throws IOException {
+		if (files == null)
+			return;
+
+		DefaultListModel lista = new DefaultListModel<>();
 		lista = new DefaultListModel<>();
-		
+
 		list.setForeground(Color.blue);
 		list.setFont(new Font("Courier", Font.PLAIN, 12));
-		
+
 		list.removeAll();
+
+		lista.addElement(direc2);
+
+		for (int i = 0; i < files.length; i++) {
+			if (!(files[i].getName()).equals(".") && !(files[i].getName()).equals("..")) {
+				// Se obtiene el nombre del fichero o directorio
+				String f = files[i].getName();
+				// Si es directorio se a�ade al nombre (DIR)
+				if (files[i].isDirectory()) {
+					f = "(Carpeta) " + f;
+				}
+				lista.addElement(f);
+
+			}
+		}
+		list.setModel(lista);
 	}
 }
